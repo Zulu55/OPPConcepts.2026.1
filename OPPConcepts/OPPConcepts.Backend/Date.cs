dd1df56d4fae52e6837c8d87f1a2e9d22d6e1673
Juan Zuluaga <juan.zuluaga@empactit.com>
2026-02-19 20:03:35 -0500

    Add employee class

--- OPPConcepts/OPPConcepts.Backend/Date.cs
+++ OPPConcepts/OPPConcepts.Backend/Date.cs
@@ -2,10 +2,12 @@
 
 public class Date
 {
+    // Fields
     private int _year;
     private int _month;
     private int _day;
 
+    // Constructors
     public Date()
     {
         _year = 1900;
@@ -20,6 +22,7 @@ public class Date
         Day = day;
     }
 
+    // Properties
     public int Year
     {
         get => _year;
@@ -38,6 +41,7 @@ public class Date
         set => _day = ValidateDay(value);
     }
 
+    // Methods
     public override string ToString()
     {
         return $"{Year:0000}/{Month:00}/{Day:00}";
