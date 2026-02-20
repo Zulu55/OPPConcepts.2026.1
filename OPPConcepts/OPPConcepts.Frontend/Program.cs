30efcf1141a65e9843526926cdc78455edbae75a
Juan Zuluaga <juan.zuluaga@empactit.com>
2026-02-19 20:26:50 -0500

    Add salary employee

--- OPPConcepts/OPPConcepts.Frontend/Program.cs
+++ OPPConcepts/OPPConcepts.Frontend/Program.cs
@@ -2,6 +2,10 @@
 
 try
 {
+    var employee1 = new SalaryEmployee(1010, "Maria", "Perez", true, new Date(1990, 5, 15), new Date(2020, 1, 1), 2500000);
+    var employee2 = new SalaryEmployee(2020, "Joaquin", "Gonzalez", true, new Date(1980, 3, 5), new Date(2016, 11, 16), 10395876);
+    Console.WriteLine(employee1);
+    Console.WriteLine(employee2);
 }
 catch (Exception ex)
 {
